f55b4390f36d2a07613374f9c52a7032c4655d7f
Ytalo Santos <ytalos762@gmail.com>
2025-10-10 20:53:50 -0300

    removing sensitive data

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ using TheKeySystem.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.s
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TksDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("TksDefaultConnection")));
+builder.Services.AddDbContext<TksDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
